27c4bc4ce6df4288cf214a2b8c9783fe9937e76d
sidz1235 <128635081+sidz1235@users.noreply.github.com>
2023-09-20 00:48:47 +0530

    Add files via upload

---
+++ scripts/binary.py
@@ -0,0 +1,9 @@
+declaration:
+binarysearchtree<int>b1(3)
+init:
+
+code:
+b1.insert(3)
+b1.insert(2)
+b1.insert(5)
+b1.in_order()
---
+++ scripts/dq.py
@@ -0,0 +1,6 @@
+declaration:
+binarysearchtree<str>b1()
+init:
+b1=[a,d,r,s]
+code:
+
